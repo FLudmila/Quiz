05d026db0be4f424e69fda85d933016efe030e4a
Frolova Ludmila <frolova.ludmila@russianpost.ru>
2022-08-04 23:36:15 +0300

    Написана программа

---
+++ Code/Program.cs
@@ -0,0 +1,49 @@
+﻿//Написать программу, которая из имеющегося массива строк
+//формирует массив из строк, длина которых меньше либо равна 3 символам.
+
+void Zadacha1()
+{
+    Random rand = new Random();
+    int numb = rand.Next(3,8);
+    string[] array = new string[numb];
+
+    Console.WriteLine("Заполните массив строками:");
+    FillArray(array);
+    Console.WriteLine("Первичный массив:");
+    PrintArray(array);
+    Console.WriteLine();
+    Console.WriteLine("Вторичный массив:");
+    SortArray(array);
+}
+      
+string[] FillArray(string[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = Console.ReadLine();
+    }
+    return arr;
+}
+        
+string[] PrintArray(string[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        Console.Write(arr[i] + "\t");
+    }
+    return arr;
+}
+
+void SortArray(string[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (arr[i].Length <= 3)
+        { 
+            Console.Write(arr[i] + "\t");
+        }
+    }
+    Console.WriteLine();
+}
+
+Zadacha1();
